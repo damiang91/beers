8d80b2d9dbe1d395797f9d833554ef51f452064c
Damian Gurgul <damian.gurgul91@gmail.com>
2019-06-02 16:12:46 +0200

    Updates

---
+++ precache-manifest.a3a56e3d133d8d71a748e8d87eecb3e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e61790e10790ef16a7b9973d17d4864b",
+    "url": "/beers/index.html"
+  },
+  {
+    "revision": "60bcb24c485fdb00e4ef",
+    "url": "/beers/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "446f5a9882f0f2dc329b",
+    "url": "/beers/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "60bcb24c485fdb00e4ef",
+    "url": "/beers/static/js/2.cb8c4f8d.chunk.js"
+  },
+  {
+    "revision": "446f5a9882f0f2dc329b",
+    "url": "/beers/static/js/main.43896dca.chunk.js"
+  },
+  {
+    "revision": "e4ad694ab96731d20d82",
+    "url": "/beers/static/js/runtime~main.fb6b770c.js"
+  }
+]);
